c2b2db98d387db9c8e68659a36d5c9736847aaf2
Mikkel <58807701+professorcrazy@users.noreply.github.com>
2025-02-17 13:36:45 +0100

    Added Waypints

--- Assets/Scripts/GuardController.cs
+++ Assets/Scripts/GuardController.cs
@@ -2,18 +2,27 @@ using UnityEngine;
 using UnityEngine.AI;
 public class GuardController : MonoBehaviour
 {
-    NavMeshAgent agent;
-    public Transform target;
+    private NavMeshAgent agent;
+    public Transform[] waypoints;
+    private int currentWaypoint = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(target.position);
+        if (waypoints.Length >= 2)
+        {
+            transform.position = waypoints[0].position;
+            agent.SetDestination(waypoints[currentWaypoint].position);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is called once per physics update (standard 50 times per second)
+    void FixedUpdate()
     {
-        
+        if (agent.remainingDistance <= 0.1f)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+            agent.SetDestination(waypoints[currentWaypoint].position);
+        }
     }
 }
